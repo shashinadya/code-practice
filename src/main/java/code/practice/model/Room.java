d446abf6ea29710f9eff7976ab74c2807ef2985e
shashinadya <67606000+shashinadya@users.noreply.github.com>
2024-02-01 23:44:38 -0800

    RoomService task is done. Tests added (#2)
    RoomService task is done. Tests added.

---
+++ src/main/java/code/practice/model/Room.java
@@ -0,0 +1,62 @@
+package code.practice.model;
+
+import java.util.Objects;
+
+public class Room {
+    private String name;
+    private String type;
+    private int capacity;
+    private double rate;
+
+    public Room(String name, String type, int capacity, double rate) {
+        this.name = name;
+        this.type = type;
+        this.capacity = capacity;
+        this.rate = rate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Room room = (Room) o;
+        return capacity == room.capacity && Double.compare(rate, room.rate) == 0 && Objects.equals(name, room.name) && Objects.equals(type, room.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, type, capacity, rate);
+    }
+}
---
+++ src/test/java/code/practice/tasks/RoomServiceTestChapter2.java
@@ -0,0 +1,59 @@
+package code.practice.tasks;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Collection;
+
+import code.practice.model.Room;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RoomServiceTestChapter2 {
+    private RoomService service;
+
+    private Room cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
+    private Room manchester = new Room("Manchester", "Suite", 5, 250.00);
+    private Room piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.00);
+    private Room oxford = new Room("Oxford", "Suite", 5, 225.0);
+    private Room victoria = new Room("Victoria", "Suite", 5, 225.00);
+    private Room westminster = new Room("Westminster", "Premiere Room", 4, 200.00);
+
+    @BeforeEach
+    void setUp() {
+        service = new RoomService();
+
+        service.createRoom("Piccadilly", "Guest Room", 3, 125.00);
+        service.createRoom("Cambridge", "Premiere Room", 4, 175.00);
+        service.createRoom("Victoria", "Suite", 5, 225.00);
+        service.createRoom("Westminster", "Premiere Room", 4, 200.00);
+    }
+
+    @Test
+    void testHasRoom() {
+        assertFalse(service.hasRoom(manchester));
+        assertTrue(service.hasRoom(cambridge));
+    }
+
+    @Test
+    void testAsArray() {
+        Room[] rooms = service.asArray();
+
+        assertEquals(4, rooms.length);
+        assertEquals(piccadilly, rooms[0]);
+        assertEquals(cambridge, rooms[1]);
+        assertEquals(victoria, rooms[2]);
+        assertEquals(westminster, rooms[3]);
+    }
+
+    @Test
+    void testGetByType() {
+        Collection<Room> guestRooms = service.getByType("Premiere Room");
+
+        assertEquals(2, guestRooms.size());
+        assertTrue(guestRooms.stream()
+                .allMatch(r -> r.getType().equals("Premiere Room")));
+        assertEquals(4, service.getInventory().size());
+    }
+}
