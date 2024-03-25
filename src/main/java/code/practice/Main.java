c6f3f655a8f692cc04aae648d45d4463b17662f2
Nadya <shashinadya@gmail.com>
2024-03-24 23:20:27 -0700

    Removed unused imports

--- src/main/java/code/practice/Main.java
+++ src/main/java/code/practice/Main.java
@@ -1,8 +1,5 @@
 package code.practice;
 
-import oop.animals.enclosures.Enclosure;
-import oop.animals.mammals.artiodactyls.Cow;
-
 public class Main {
 
     public static void main(String[] args) {
