7a400d0ffb7d5bae0b5a734b910279241f4fe62a
Nadya <shashinadya@gmail.com>
2024-10-01 13:49:27 -0700

    Changed deleteTable implementation for Json database.

--- database/src/main/java/database/Main.java
+++ database/src/main/java/database/Main.java
@@ -2,6 +2,7 @@ package database;
 
 import database.controller.DatabaseServiceRestController;
 import database.controller.DatabaseControllerExceptionHandler;
+import database.service.JsonDatabaseService;
 import database.service.SqlDatabaseService;
 import io.javalin.Javalin;
 
@@ -10,8 +11,9 @@ import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        final var databaseService = new SqlDatabaseService("jdbc:mysql://localhost:3306/",
-                "db_user", "Qwerty!1", "entities", "application.properties");
+//        final var databaseService = new SqlDatabaseService("jdbc:mysql://localhost:3306/",
+//                "db_user", "Qwerty!1", "entities", "application.properties");
+        final var databaseService = new JsonDatabaseService("application.properties");
         final var dbServiceRestController = new DatabaseServiceRestController(databaseService);
         var app = Javalin.create(dbServiceRestController::configureRouter)
                 .start();
