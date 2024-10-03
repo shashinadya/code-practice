daefb38c21b9a7d9ed2918d737f247ae98602e19
mishashin <mishashin@yandex.ru>
2024-10-03 14:42:55 -0700

    Added Swagger API documentation. (#58)
    Co-authored-by: Mikhail Shashin <tolomuist@gmail.com>

--- database/src/main/java/database/Main.java
+++ database/src/main/java/database/Main.java
@@ -2,23 +2,35 @@ package database;
 
 import database.controller.DatabaseServiceRestController;
 import database.controller.DatabaseControllerExceptionHandler;
-import database.service.JsonDatabaseService;
+import database.helper.Swagger;
+import database.helper.Utils;
 import database.service.SqlDatabaseService;
 import io.javalin.Javalin;
-
-import java.sql.SQLException;
+import io.javalin.openapi.plugin.OpenApiPlugin;
+import io.javalin.openapi.plugin.swagger.SwaggerPlugin;
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException {
-//        final var databaseService = new SqlDatabaseService("jdbc:mysql://localhost:3306/",
-//                "db_user", "Qwerty!1", "entities", "application.properties");
-        final var databaseService = new JsonDatabaseService("application.properties");
-        final var dbServiceRestController = new DatabaseServiceRestController(databaseService);
-        var app = Javalin.create(dbServiceRestController::configureRouter)
-                .start();
+    public static void main(String[] args) {
+        final var entities = Utils.getSubclassesOfBaseEntity();
+        var app = Javalin.create(config -> {
+            config.registerPlugin(new OpenApiPlugin(pluginConfig ->
+                    pluginConfig.withDefinitionConfiguration((version, definition) ->
+                            definition.withInfo(info -> {
+                                info.setDescription(Swagger.buildDescription(entities));
+                                info.setTitle(Swagger.TITLE);
+                            }))));
+            config.registerPlugin(new SwaggerPlugin());
 
-        DatabaseControllerExceptionHandler fileDatabaseExceptionHandler = new DatabaseControllerExceptionHandler();
-        fileDatabaseExceptionHandler.register(app);
+            final var databaseService = new SqlDatabaseService("jdbc:mysql://localhost:3306/",
+                    "db_user", "Qwerty!1", "entities", "application.properties");
+            final var dbServiceRestController = new DatabaseServiceRestController(databaseService, entities);
+            dbServiceRestController.configureRouter(config);
+        }).start();
+
+        var databaseControllerExceptionHandler = new DatabaseControllerExceptionHandler();
+        databaseControllerExceptionHandler.register(app);
+
+        System.out.println("Check out Swagger UI docs at http://localhost:8080/swagger");
     }
 }
--- database/src/main/java/database/controller/DatabaseServiceRestController.java
+++ database/src/main/java/database/controller/DatabaseServiceRestController.java
@@ -8,9 +8,16 @@ import database.service.DatabaseService;
 import io.javalin.config.JavalinConfig;
 import io.javalin.http.Context;
 import io.javalin.http.NotFoundResponse;
+import io.javalin.openapi.HttpMethod;
+import io.javalin.openapi.OpenApi;
+import io.javalin.openapi.OpenApiContent;
+import io.javalin.openapi.OpenApiParam;
+import io.javalin.openapi.OpenApiRequestBody;
+import io.javalin.openapi.OpenApiResponse;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static io.javalin.apibuilder.ApiBuilder.delete;
 import static io.javalin.apibuilder.ApiBuilder.get;
@@ -19,24 +26,15 @@ import static io.javalin.apibuilder.ApiBuilder.post;
 import static io.javalin.apibuilder.ApiBuilder.put;
 
 public class DatabaseServiceRestController {
-    private DatabaseService databaseService;
+    private final DatabaseService databaseService;
+    private final Set<Class<? extends BaseEntity>> entities;
     static final String ID_PARAMETER_NAME = "id";
     static final String INVALID_PARAM_VALUE = "Invalid value for limit or offset parameter. They must be integers.";
     static final String INVALID_ID_VALUE = "Invalid value for id parameter. It must be integer.";
 
-    public DatabaseServiceRestController() {
-    }
-
-    public DatabaseServiceRestController(DatabaseService databaseService) {
+    public DatabaseServiceRestController(DatabaseService databaseService, Set<Class<? extends BaseEntity>> entities) {
         this.databaseService = databaseService;
-    }
-
-    public DatabaseService getDatabaseService() {
-        return databaseService;
-    }
-
-    public void setDatabaseService(DatabaseService databaseService) {
-        this.databaseService = databaseService;
+        this.entities = entities;
     }
 
     public void configureRouter(JavalinConfig config) {
@@ -58,16 +56,66 @@ public class DatabaseServiceRestController {
                 }));
     }
 
+    @OpenApi(
+            summary = "Create new table",
+            operationId = "createTable",
+            path = "/api/v1/database/{entityClass}/table",
+            methods = HttpMethod.POST,
+            pathParams = {
+                    @OpenApiParam(name = "entityClass", description = "Class of the entity", required = true)
+            },
+            responses = {
+                    @OpenApiResponse(status = "200", description = "Table created",
+                            content = @OpenApiContent(from = Boolean.class)),
+                    @OpenApiResponse(status = "400", description = "Invalid entity class"),
+                    @OpenApiResponse(status = "500", description = "Unable to create table")
+            }
+    )
     void handleCreateTable(Context ctx) {
         Class<? extends BaseEntity> entityClass = getClassFromPath(ctx);
         ctx.json(databaseService.createTable(entityClass));
     }
 
+    @OpenApi(
+            summary = "Delete table",
+            operationId = "deleteTable",
+            path = "/api/v1/database/{entityClass}/table",
+            methods = HttpMethod.DELETE,
+            pathParams = {
+                    @OpenApiParam(name = "entityClass", description = "Class of the entity", required = true)
+            },
+            responses = {
+                    @OpenApiResponse(status = "200", description = "Table deleted successfully",
+                            content = @OpenApiContent(from = Boolean.class)),
+                    @OpenApiResponse(status = "400", description = "Invalid entity class"),
+                    @OpenApiResponse(status = "500", description = "Unable to delete table")
+            }
+    )
     void handleDeleteTable(Context ctx) {
         Class<? extends BaseEntity> entityClass = getClassFromPath(ctx);
         ctx.json(databaseService.deleteTable(entityClass));
     }
 
+    @OpenApi(
+            summary = "Add new record to table",
+            operationId = "addNewRecord",
+            path = "/api/v1/database/{entityClass}",
+            methods = HttpMethod.POST,
+            pathParams = {
+                    @OpenApiParam(name = "entityClass", description = "Class of the entity", required = true)
+            },
+            requestBody = @OpenApiRequestBody(
+                    content = @OpenApiContent(from = BaseEntity.class),
+                    description = "New record to be added",
+                    required = true
+            ),
+            responses = {
+                    @OpenApiResponse(status = "200", description = "Record added successfully",
+                            content = @OpenApiContent(from = BaseEntity.class)),
+                    @OpenApiResponse(status = "400", description = "Invalid entity class"),
+                    @OpenApiResponse(status = "500", description = "Invalid entity data or unable to create record")
+            }
+    )
     void handleAddNewRecord(Context ctx) {
         Class<? extends BaseEntity> entityClass = getClassFromPath(ctx);
         var entity = ctx.bodyAsClass(entityClass);
@@ -82,6 +130,26 @@ public class DatabaseServiceRestController {
         ctx.json(result);
     }
 
+    @OpenApi(
+            summary = "Get all records",
+            operationId = "getAllRecords",
+            path = "/api/v1/database/{entityClass}",
+            methods = HttpMethod.GET,
+            pathParams = {
+                    @OpenApiParam(name = "entityClass", description = "Class of the entity", required = true)
+            },
+            queryParams = {
+                    @OpenApiParam(name = "limit", description = "Maximum records count to be retrieved"),
+                    @OpenApiParam(name = "offset", description = "Offset value from the first record")
+            },
+            responses = {
+                    @OpenApiResponse(status = "200", description = "List of all records",
+                            content = @OpenApiContent(from = BaseEntity[].class)),
+                    @OpenApiResponse(status = "400", description = "Invalid entity class"),
+                    @OpenApiResponse(status = "400", description = "Invalid id, limit or offset format"),
+                    @OpenApiResponse(status = "500", description = "Unable to retrieve records")
+            }
+    )
     void handleGetAllRecords(Context ctx) {
         Class<? extends BaseEntity> entityClass = getClassFromPath(ctx);
         String limitParam = ctx.queryParam("limit");
@@ -101,6 +169,29 @@ public class DatabaseServiceRestController {
         }
     }
 
+    @OpenApi(
+            summary = "Update existing record",
+            operationId = "updateRecord",
+            path = "/api/v1/database/{entityClass}/{id}",
+            methods = HttpMethod.PUT,
+            pathParams = {
+                    @OpenApiParam(name = "entityClass", description = "Class of the entity", required = true),
+                    @OpenApiParam(name = "id", description = "Id of the record to update", required = true)
+            },
+            requestBody = @OpenApiRequestBody(
+                    content = @OpenApiContent(from = BaseEntity.class),
+                    description = "Updated record data",
+                    required = true
+            ),
+            responses = {
+                    @OpenApiResponse(status = "200", description = "Record updated successfully",
+                            content = @OpenApiContent(from = BaseEntity.class)),
+                    @OpenApiResponse(status = "400", description = "Invalid entity class or record does not exist"),
+                    @OpenApiResponse(status = "400", description = "Invalid id format"),
+                    @OpenApiResponse(status = "400", description = "Id in path mismatches id from given entity"),
+                    @OpenApiResponse(status = "500", description = "Invalid entity data or unable to update record")
+            }
+    )
     void handleUpdateRecord(Context ctx) {
         Class<? extends BaseEntity> entityClass = getClassFromPath(ctx);
         Integer pathId;
@@ -117,6 +208,21 @@ public class DatabaseServiceRestController {
         ctx.json(databaseService.updateRecordInTable(entity, pathId));
     }
 
+    @OpenApi(
+            summary = "Remove record",
+            operationId = "removeRecord",
+            path = "/api/v1/database/{entityClass}/{id}",
+            methods = HttpMethod.DELETE,
+            pathParams = {
+                    @OpenApiParam(name = "entityClass", description = "Class of the entity", required = true),
+                    @OpenApiParam(name = "id", description = "Id of the record to be removed", required = true)
+            },
+            responses = {
+                    @OpenApiResponse(status = "200", description = "Record removed successfully"),
+                    @OpenApiResponse(status = "400", description = "Invalid entity class or invalid id format"),
+                    @OpenApiResponse(status = "500", description = "Unable to remove record")
+            }
+    )
     void handleRemoveRecord(Context ctx) {
         Class<? extends BaseEntity> entityClass = getClassFromPath(ctx);
         try {
@@ -127,6 +233,23 @@ public class DatabaseServiceRestController {
         }
     }
 
+    @OpenApi(
+            summary = "Get record by id",
+            operationId = "getById",
+            path = "/api/v1/database/{entityClass}/{id}",
+            methods = HttpMethod.GET,
+            pathParams = {
+                    @OpenApiParam(name = "entityClass", description = "Class of the entity", required = true),
+                    @OpenApiParam(name = "id", description = "Id of the record to retrieve", required = true)
+            },
+            responses = {
+                    @OpenApiResponse(status = "200", description = "Record retrieved successfully",
+                            content = @OpenApiContent(from = BaseEntity.class)),
+                    @OpenApiResponse(status = "400", description = "Invalid entity class or invalid id format"),
+                    @OpenApiResponse(status = "404", description = "Record not found"),
+                    @OpenApiResponse(status = "500", description = "Unable to retrieve record")
+            }
+    )
     void handleGetById(Context ctx) {
         Class<? extends BaseEntity> entityClass = getClassFromPath(ctx);
         int id;
@@ -142,6 +265,20 @@ public class DatabaseServiceRestController {
         ctx.json(entity);
     }
 
+    @OpenApi(
+            summary = "Remove all records",
+            operationId = "removeAllRecords",
+            path = "/api/v1/database/{entityClass}",
+            methods = HttpMethod.DELETE,
+            pathParams = {
+                    @OpenApiParam(name = "entityClass", description = "Class of the entity", required = true)
+            },
+            responses = {
+                    @OpenApiResponse(status = "200", description = "All records removed successfully"),
+                    @OpenApiResponse(status = "400", description = "Invalid entity class"),
+                    @OpenApiResponse(status = "500", description = "Unable to remove all records")
+            }
+    )
     void handleRemoveAllRecords(Context ctx) {
         Class<? extends BaseEntity> entityClass = getClassFromPath(ctx);
         databaseService.removeAllRecordsFromTable(entityClass);
@@ -149,11 +286,9 @@ public class DatabaseServiceRestController {
 
     private Class<? extends BaseEntity> getClassFromPath(Context ctx) {
         String entityClassName = ctx.pathParam("entityClass");
-        try {
-            String className = "database.entity." + entityClassName;
-            return (Class<? extends BaseEntity>) Class.forName(className);
-        } catch (ClassNotFoundException e) {
-            throw new BadRequestException("Invalid database.entity class: " + entityClassName);
-        }
+        return entities.stream()
+                .filter(entity -> entity.getSimpleName().equalsIgnoreCase(entityClassName))
+                .findAny()
+                .orElseThrow(() -> new BadRequestException("Invalid database.entity class: " + entityClassName));
     }
 }
