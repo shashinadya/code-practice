9ca46574531c8ce69a66c7aa48e0b00a4d2ac6e3
Nadya <shashinadya@gmail.com>
2024-11-05 14:03:22 -0800

    Multithreading task

--- practice/src/main/java/practice/Main.java
+++ practice/src/main/java/practice/Main.java
@@ -2,9 +2,13 @@ package practice;
 
 import practice.model.Account;
 import practice.tasks.AccountService;
+import practice.tasks.BankAccount;
 import practice.tasks.MultithreadingTask;
 
 public class Main {
+    static Account account = new Account(1, 1000);
+    static int amountToChange = 200;
+    static BankAccount bankAccount = new BankAccount();
 
     public static void main(String[] args) throws InterruptedException {
         MultithreadingTask task = new MultithreadingTask();
@@ -21,5 +25,29 @@ public class Main {
         accountService.moveMoney(acc1, acc2, 200);
         System.out.println(acc1.getMoneyAmount());
         System.out.println(acc2.getMoneyAmount());
+
+        MyThreadDeposit myThreadDeposit1 = new MyThreadDeposit();
+        MyThreadDeposit myThreadDeposit2 = new MyThreadDeposit();
+        MyThreadWithdraw myThreadWithdraw1 = new MyThreadWithdraw();
+        MyThreadWithdraw myThreadWithdraw2 = new MyThreadWithdraw();
+
+        myThreadDeposit1.start();
+        myThreadWithdraw1.start();
+        myThreadDeposit2.start();
+        myThreadWithdraw2.start();
+    }
+
+    static class MyThreadDeposit extends Thread {
+        @Override
+        public void run() {
+            bankAccount.deposit(account, amountToChange);
+        }
+    }
+
+    static class MyThreadWithdraw extends Thread {
+        @Override
+        public void run() {
+            bankAccount.withdraw(account, amountToChange);
+        }
     }
 }
---
+++ practice/src/main/java/practice/tasks/BankAccount.java
@@ -0,0 +1,31 @@
+package practice.tasks;
+
+import practice.model.Account;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BankAccount {
+    Lock lock = new ReentrantLock();
+
+    public void deposit(Account account, int amount) {
+        try {
+            lock.lock();
+            account.setMoneyAmount(account.getMoneyAmount() + amount);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void withdraw(Account account, int amount) {
+        try {
+            lock.lock();
+            if (amount > account.getMoneyAmount()) {
+                lock.unlock();
+            }
+            account.setMoneyAmount(account.getMoneyAmount() - amount);
+        } finally {
+            lock.unlock();
+        }
+    }
+}
